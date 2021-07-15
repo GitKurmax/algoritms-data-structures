564772d991c7f2eaac036d776cd46e99e614fb0c
Alex <alexandrmail32@gmail.com>
2021-07-15 14:41:55 +0300

    Added selection sort

---
+++ sort/selectionSort.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+    for(let i=0 ; i < arr.length - 1; i++) {
+        let min = arr[i]
+        for(let j=i+1; j < arr.length; j++) {
+            if(min > arr[j]) {
+                swap(arr, i, j)
+                min = arr[i]
+            }
+        }
+    }
+    return arr
+}
+
+function swap(arr, firstElemPosition, secondElemPosition) {
+    const [first, second, ] = [arr[secondElemPosition], arr[firstElemPosition]]
+    arr[firstElemPosition] = first
+    arr[secondElemPosition] = second
+}
+
+console.log(selectionSort([234, 43, 323, 5002, 2, 99, 345, 0, 50, 302]))
