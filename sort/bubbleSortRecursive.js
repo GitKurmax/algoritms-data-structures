a1ebdd89c8c16603d4cbff7c76501dbf58a93166
Alex <alexandrmail32@gmail.com>
2021-07-15 15:48:09 +0300

    Added bubble sort recursive

--- sort/bubbleSortRecursive.js
+++ sort/bubbleSortRecursive.js
@@ -1,7 +1,7 @@
-function bubbleSort(arr) {
-    for(let i = 0; i < arr.length - 1; i++) {
+function bubbleSortRecursive(arr, index) {
         let swap = false;
-        for(let j = 1; j < arr.length - i; j++) {
+
+        for(let j = 1; j < arr.length - index; j++) {
             if(arr[j-1] > arr[j]) {
                 const elem = arr[j-1]
                 arr[j-1] = arr[j]
@@ -12,9 +12,7 @@ function bubbleSort(arr) {
         if(!swap) {
             return arr
         }
-    }
-
+    return bubbleSortRecursive(arr, index + 1)
 }
 
-console.log(bubbleSort([234, 43, 323, 5002, 2, 99, 345, 0, 50, 302]))
-
+console.log(bubbleSortRecursive([234, 43, 323, 5002, 2, 99, 345, 0, 50, 302], 0))
