cdf87bb712280c42f5f5b0e1f3ab6a8525fd5c64
Alex <alexandrmail32@gmail.com>
2022-01-06 12:36:39 +0200

    Changed selection sort

--- sort/selectionSort.js
+++ sort/selectionSort.js
@@ -1,18 +1,17 @@
+// Complexity O(n2)
+
 function selectionSort(arr) {
     for(let i=0 ; i < arr.length - 1; i++) {
-        let min = arr[i]
+        let minIndex = i
         for(let j=i+1; j < arr.length; j++) {
-            if(min > arr[j]) {
-                swap(arr, i, j)
-                min = arr[i]
+            if(arr[minIndex] > arr[j]) {
+                minIndex = j
             }
         }
+
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
     }
     return arr
-}
-
-function swap(arr, firstElemPosition, secondElemPosition) {
-   [arr[firstElemPosition], arr[secondElemPosition]] = [arr[secondElemPosition], arr[firstElemPosition]]
 }
 
 console.log(selectionSort([234, 43, 323, 5002, 2, 99, 345, 0, 50, 302]))
